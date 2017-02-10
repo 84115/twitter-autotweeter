738e4bf35cd1e106d3898b8efba4845c52aca367
James <jamesballsemail@gmail.com>
2017-02-10 22:04:31 +0000

    Added Missing Auto Commit

--- cron-on.sh
+++ cron-on.sh
@@ -1,4 +1,6 @@
 git pull origin master
 echo -n "" > stamp.json
 ./tweet.sh tweet "Clocked in @ "$(date +"%H:%M")" http://github.com/84115/twooo" >> stamp.json 
+git add .
+git commit am "Clocked in @ "$(date +"%H:%M")""
 git push origin master
